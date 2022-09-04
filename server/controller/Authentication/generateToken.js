8d15a176fab3c407beba879b5a411bd7e0edfead
Mohammed alagha <modyagha7@gmail.com>
2022-09-04 22:37:32 +0300

    make a function to generate token and store it in the browser cookies relates #15

---
+++ server/controller/Authentication/generateToken.js
@@ -0,0 +1,12 @@
+const { sign } = require("jsonwebtoken");
+require("dotenv").config();
+const generateToken = (req, res, data) => {
+  sign(data, process.env.SECRET_KEY, { algorithm: "HS256" }, (err, encoded) => {
+    if (err) console.log(err);
+    else {
+      res.cookie("token", encoded).redirect("/");
+    }
+  });
+};
+
+module.exports = generateToken;
