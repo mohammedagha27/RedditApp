b95b909cd7f52e5a7edfeeed95292b7a941c376f
Mohammed alagha <modyagha7@gmail.com>
2022-09-04 21:38:54 +0300

    create a function to validate the signup inputs relates #11

---
+++ server/controller/Authentication/signupValidation.js
@@ -0,0 +1,21 @@
+const Joi = require("joi");
+const signupValidation = (data) => {
+  const schema = Joi.object({
+    email: Joi.string()
+      .email({
+        minDomainSegments: 2,
+        tlds: { allow: ["com", "net"] },
+      })
+      .required(),
+    username: Joi.string().min(3).max(30).required(),
+    password: Joi.string()
+      .min(6)
+      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+      .required(),
+    confirmPassword: Joi.ref("password"),
+  });
+  const validation = schema.validate(data, { abortEarly: false });
+  return validation;
+};
+
+module.exports = signupValidation;
---
+++ server/database/queries/checkNewUserDataQ.js
@@ -0,0 +1,10 @@
+const connection = require("../config/connection");
+
+const checkNewUserDataQ = (email, username) => {
+  return connection.query(
+    "select * from users where email=$1 or username= $2",
+    [email, username]
+  );
+};
+
+module.exports = checkNewUserDataQ;
