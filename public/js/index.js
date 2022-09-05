07aeeeb46868e110a91b7956c783acbe71d3d75a
Mohammed alagha <modyagha7@gmail.com>
2022-09-05 15:53:06 +0300

    style the signup form close #23

--- public/js/index.js
+++ public/js/index.js
@@ -4,10 +4,20 @@ const searchInput = document.querySelector(
 const searchForm = document.querySelector(
   "header .header-container .search form"
 );
+const signUpBtn = document.querySelector(" .btns #signupBtn");
+const signUpPop = document.querySelector(".signup-popup");
+const signUpPopCloseBtn = document.querySelector(".signup-popup .signup-close");
 
 searchInput.addEventListener("focus", (e) => {
   searchForm.style.borderColor = "var(--button)";
 });
 searchInput.addEventListener("focusout", (e) => {
   searchForm.style.borderColor = "var(--line)";
+});
+
+signUpBtn.addEventListener("click", (e) => {
+  signUpPop.classList.add("active");
+});
+signUpPopCloseBtn.addEventListener("click", (e) => {
+  signUpPop.classList.remove("active");
 });
