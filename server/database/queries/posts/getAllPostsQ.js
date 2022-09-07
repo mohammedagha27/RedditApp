0036520b09dde3ce85db33620642122c32547d5f
Mohammed alagha <modyagha7@gmail.com>
2022-09-07 14:20:06 +0300

    sort the posts by thier votes

--- server/database/queries/posts/getAllPostsQ.js
+++ server/database/queries/posts/getAllPostsQ.js
@@ -5,7 +5,7 @@ const getAllPostsQ = () => {
     `select
       p.id, p.title, p.content, p.posted_at,
       u.username as user_name, u.img_url as user_img,
-      SUM (v.vote) as votes_sum 
+      SUM (COALESCE(v.vote,0)) as votes_sum 
     from
       posts p left join votes v
         on p.id = v.post_id
