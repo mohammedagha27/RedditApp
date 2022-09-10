c7617c6e9f8f28322d50fadfb2cb312f021aee79
Mohammed alagha <modyagha7@gmail.com>
2022-09-10 20:14:12 +0300

    create the previous posts section.

---
+++ server/database/queries/posts/getUserPostsQ.js
@@ -0,0 +1,17 @@
+const connection = require("../../config/connection");
+
+const getUserPostsQ = (id) => {
+  return connection.query(
+    `select
+      p.*,
+      u.username as user_name, u.img_url as user_img
+    from
+      posts p 
+        join users u
+        on u.id =p.user_id
+         where u.id =$1`,
+    [id]
+  );
+};
+
+module.exports = getUserPostsQ;
--- server/database/queries/posts/index.js
+++ server/database/queries/posts/index.js
@@ -2,5 +2,12 @@ const getAllPostsQ = require("./getAllPostsQ");
 const addNewPostQ = require("./addNewPostQ");
 const addNewPostMediaQ = require("./addNewPostMediaQ");
 const deletePostQ = require("./deletePostQ");
+const getUserPostsQ = require("./getUserPostsQ");
 
-module.exports = { getAllPostsQ, addNewPostQ, addNewPostMediaQ, deletePostQ };
+module.exports = {
+  getAllPostsQ,
+  addNewPostQ,
+  addNewPostMediaQ,
+  deletePostQ,
+  getUserPostsQ,
+};
